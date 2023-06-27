069bd441c8b23e9e60700b80a3ec6420a2dc4a94
CyberCowboy23 <134964090+cyberkyle23@users.noreply.github.com>
2023-06-27 10:15:05 -0400

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# prints or echos out text
+print("I will now count my chickens:")
+# outputs "Hens" and solves the math problem.  This is outside of the ""
+print("Hens", 25 + 30 / 6)
+# outputs "Roosters" and solves the math problem.
+print("Roosters", 100 - 25 * 3 % 4)
+# outputs the text
+print("Now I will count the eggs:")
+# solves the math problem
+print(3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6)
+# outputs the line as text.  It did not solve the problem
+print("Is it true that 3 + 2 < 5 - 7?")
+# this printed out "False" this shows that it gave a logical answer to the problem using the <
+print(3 + 2 < 5 - 7)
+# the "" outputs texts, the "," sepearates the next line and the math problem is solved and outputs.
+print("What is 3 + 2?", 3 + 2)
+# Same as above
+print("What is 5 - 7?", 5 - 7)
+# outputs text
+print("Oh, that's why it's false")
+# same as above
+print("How about some more.")
+# outputs text and solves logical question with a true or false claim
+print("Is it greater?", 5 > 2)
+# same as above
+print("Is it greater or equal?", 5 >= -2)
+# same as above
+print("Is it less or equal?", 5 <= -2)
